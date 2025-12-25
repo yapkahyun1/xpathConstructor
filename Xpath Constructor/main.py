2dd487459d193588ffec77a1622a4337f9324033
yap <kahyun.yap@ppafuxion.com>
2025-12-25 21:52:12 +0800

    added webscraper.py

--- Xpath Constructor/main.py
+++ Xpath Constructor/main.py
@@ -1,5 +1,7 @@
 import json
 import os
+from playwright.sync_api import sync_playwright
+from web_scraper import scrape_elements
 
 AbsPath = "html/body/cimb-app/div/page-login/div[2]/div/div[4]/div/form/div[2]/div[2]/div/input"
 print("Absolute Path:", AbsPath)
@@ -25,24 +27,40 @@ except json.JSONDecodeError:
 
 
 
+# Usage
+url = "https://www.saucedemo.com"
+selector = "//div[@id='login_credentials']"  # Change to your target elements
+scraped_data = scrape_elements(url, selector)
+
+# Save to JSON
+with open("scraped_data.json", "w", encoding="utf-8") as f:
+    json.dump(scraped_data, f, indent=2, ensure_ascii=False)
+
+print("✅ JSON file created: scraped_data.json")
+
+
+
+
+
+# 'loop through json and get what is needed'
+# for node in data.get("nodes", []):
+#     node_data = node.get("data", {})
+#     if node_data.get("stage_number") == 2:
+#         #print("JSON SOURCE HERE ----------------------------------------\n"+str(node_data))
+#         actions_data = node_data.get('actions', [])
+#         #print(actions_data)
+        
+#         for action_item in actions_data:
+#             element_data = action_item.get('element', 'no data')
+#             #print(element_data.get('xpath'))
+#             attributes_data = element_data.get('attributes', 'nothing')
+#             print(attributes_data)
+#             attributesFinal = attributes_data.get('id')
+#             relative_xpath_construct = '//' + 'input' + f"[@id='{attributesFinal}']"
+#             print("Final Output: " + relative_xpath_construct)
+#             break
     
-
-
-'loop through json and get what is needed'
-for node in data.get("nodes", []):
-    node_data = node.get("data", {})
-    if node_data.get("stage_number") == 2:
-        #print("JSON SOURCE HERE ----------------------------------------\n"+str(node_data))
-        actions_data = node_data.get('actions', [])
-        #print(actions_data)
-        for action_item in actions_data:
-            element_data = action_item.get('element', 'no data')
-            #print(element_data.get('xpath'))
-            attributes_data = element_data.get('attributes', 'nothing')
-            print(attributes_data)
-            attributesFinal = attributes_data.get('id')
-            relative_xpath_construct = '//' + 'input' + f"[@id='{attributesFinal}']"
-            print("Final Output: " + relative_xpath_construct)
+            
 
 
 
---
+++ Xpath Constructor/web_scraper.py
@@ -0,0 +1,35 @@
+
+from playwright.sync_api import sync_playwright
+
+#to scrape elements
+def scrape_elements(url, selector):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(url)
+
+        elements = page.query_selector_all(selector)
+        data = []
+
+        for el in elements:
+            attrs = {}
+            for name in el.evaluate("e => [...e.attributes].map(a => a.name)"):
+                value = el.get_attribute(name)
+                attrs[name] = value
+
+            text = el.text_content()
+            if text:
+                print('here1')
+                text = ' '.join(text.split())  # collapse whitespace
+            else:
+                print('here2')
+                text = ''
+
+            data.append({
+                "tag": el.evaluate("e => e.tagName").lower(),
+                "attrs": attrs,
+                "text": text
+            })
+
+        browser.close()
+        return data
